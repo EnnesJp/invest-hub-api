430fb3582ee04a05c9293d8f5de37becb69ddf26
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2024-01-11 12:46:38 -0300

    feat: add saving plans model and migration

---
+++ database/migrations/2024_01_11_153051_update_assets_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\SavingPlan;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->foreignIdFor(SavingPLan::class)->nullable()->after('portfolio_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->dropColumn('saving_plan_id');
+        });
+    }
+};
